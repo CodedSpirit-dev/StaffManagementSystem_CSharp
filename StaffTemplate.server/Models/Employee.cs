2d429199639549d03ea1de057c00cd7e1766a72d
CodedSpirit <viradvt@gmail.com>
2024-07-04 14:32:01 -0600

    Updating model

---
+++ StaffTemplate.server/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StaffTemplate.server.Models
+{
+    public class Employee
+    {
+        [Required]
+        public String FirstName;
+        [Required]
+        public String MiddleName;
+        [Required]
+        public String LastName;
+        [Required]
+        public String SecondLastname;
+        [Key]
+        public int SocialSecurityNumber;
+
+
+    }
+}
---
+++ StaffTemplate.server/Repository/IRepository/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace StaffTemplate.server.Repository.IRepository
+{
+    public interface IEmployeeRepository
+    {
+        ICollection<Employee> GetEmployees();
+        IEmployeeRepository GetEmployee(int id);
+        bool EmployeeExists(String name);
+        bool EmployeeExists(int id);
+        bool CreateEmployee(Employee employee);
+        bool UpdateEmployee(Employee employee);
+        bool DeleteEmployee(Employee employee);
+        bool DeleteEmployee(int id);
+        bool Save();
+
+    }
+}
