bf7fc7111c572178f3270d80fbeb86a424d0a9dc
CodedSpirit <viradvt@gmail.com>
2024-07-04 13:59:01 -0600

    Agregar archivos de proyecto.

---
+++ StaffTemplate.server/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StaffTemplate.server.Controllers
+{
+    [Route("/")]
+    [ApiController]
+
+    public class EmployeeController
+        private readonly IEmployeeRepository : ControllerBase
+    {
+    }
+}
