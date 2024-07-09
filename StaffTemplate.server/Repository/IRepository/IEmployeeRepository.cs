72614d558094577f062f1d4dc5b1fab2d7c423bd
CodedSpirit <viradvt@gmail.com>
2024-07-09 12:14:55 -0600

    Updating repository

--- StaffTemplate.server/Repository/IRepository/IEmployeeRepository.cs
+++ StaffTemplate.server/Repository/IRepository/IEmployeeRepository.cs
@@ -6,14 +6,14 @@ namespace StaffTemplate.server.Repository.IRepository
     public interface IEmployeeRepository
     {
         ICollection<Employee> GetEmployees();
-        Employee GetEmployeeById(int id);
+        Employee GetEmployeeById(int SocialSecurityNumber);
         Employee GetEmployeeBySocialSecurityNumber(int SocialSecurityNumber);
         bool EmployeeExists(string name);
         bool EmployeeExists(int SocialSecurityNumber);
         bool CreateEmployee(Employee employee);
         bool UpdateEmployee(Employee employee);
-        bool DeleteEmployee(Employee employee);
-        bool DeleteEmployee(int id);
+        bool DeleteEmployee(Employee employee, EmploymentDetails employmentDetails);
+        bool DeleteEmployee(int SocialSecurityNumber);
         bool Save();
     }
 }
