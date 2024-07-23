bcf58cc7f3aad2848d398fcbd49e2c8b2fad10bb
CodedSpirit <viradvt@gmail.com>
2024-07-23 14:17:53 -0600

    Update NavBar styling and links

--- StaffManagementSystem/staffmanagementsystem.client/src/components/NavBar/NavBar.tsx
+++ StaffManagementSystem/staffmanagementsystem.client/src/components/NavBar/NavBar.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900 items-center">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <ul className="font-medium flex flex-col flex items-center md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <div className="w-full py-3 border-b border-gray-200">
+            <ul className="ml-3 flex xl:gap-10 md:gap-8 hover:text-white gap-2 font-medium hover:text-white text-gray-900">
                 <li>
-                    <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Inicio</Link>
+                    <Link to="/" className="hover:text-white">Inicio</Link>
                 </li>
                 <li>
-                    <Link to="/lista" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Empleados</Link>
+                    <Link to="/lista" className="hover:text-white">Empleados</Link>
                 </li>
                 <li>
-                    <Link to="/crearUsuario" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Crear Empleado</Link>
+                    <Link to="/crearUsuario" className="hover:text-white">Crear Empleado</Link>
                 </li>
             </ul>
             </div>
